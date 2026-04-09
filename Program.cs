be7488cafbb8543c349f7829d8e44f09fb78a482
Dove167 <jfajardo7@my.bcit.ca>
2026-04-08 23:59:49 -0700

    Add bookstore features: book CRUD, cart, orders, sort/filter
    - Book CRUD pages (Create, Edit, Delete, Details)
    - Cart offcanvas with CartItem component
    - Orders history page with order details
    - Home page with genre filter and price sort dropdown
    - NavBar with wallet balance and cart icon
    - OrderState service for checkout flow
    - CartService singleton for cart/wallet management

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Bookstore.Data;
+using Bookstore.Services;
 using bookstore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSingleton<CartService>();
+
+builder.Services.AddScoped<OrderState>(); 
+
+// setup Database Context
+var connectionString = builder.Configuration.GetConnectionString("BOOKIESTORE_DB")
+   ?? throw new InvalidOperationException("Connection string 'BOOKIESTORE_DB' not found");
+builder.Services.AddDbContextFactory<BookstoreDb>(options => options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.EnableRetryOnFailure(
+   maxRetryCount: 10,
+   maxRetryDelay: TimeSpan.FromSeconds(30),
+   errorNumbersToAdd: null)));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
